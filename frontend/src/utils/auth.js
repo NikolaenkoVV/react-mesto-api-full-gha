2c4687d4f4498840cbe6fe2b501cd1da689bd8d0
Vitaliy Nikolaenko <korbendallasYP@gmail.com>
2023-09-19 01:57:42 +0300

    fix: url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "http://api.mestokd.students.nomoredomainsrocks.ru:3001";
 
 const checkRequestResponse = (res) => {
   if (res.ok) {
