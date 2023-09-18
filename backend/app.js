0d2e9726d79d830f2238fa30c293572e043ee70e
Vitaliy Nikolaenko <korbendallasYP@gmail.com>
2023-09-19 02:35:06 +0300

    fix: github actions #2

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const { regexLink } = require('./utils/constants');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
