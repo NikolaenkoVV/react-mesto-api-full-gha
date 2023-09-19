269b02628dbc474f757c596019f2fb5d0c491361
Vitaliy Nikolaenko <korbendallasYP@gmail.com>
2023-09-19 22:03:24 +0300

    fix: cors https

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors({ origin: 'https://mestokd.students.nomoredomainsrocks.ru' }));
+app.use(cors({ origin: 'http://mestokd.students.nomoredomainsrocks.ru' }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
