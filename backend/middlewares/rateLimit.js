61a19b907001e0648d9f441a7688fa00333bffcc
Vitaliy Nikolaenko <korbendallasYP@gmail.com>
2023-09-21 14:09:17 +0300

    package for review№2

--- backend/middlewares/rateLimit.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require("express-rate-limit");
-
-const limiter = rateLimit({
-  windowMs: 50000,
-  max: 10000,
-  message: "Количество запросов на сервер превышено",
-});
-
-module.exports = limiter;
