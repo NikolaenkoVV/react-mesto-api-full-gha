48850da61d487f2c980c13f0ce3d96e07a714725
Vitaliy Nikolaenko <korbendallasYP@gmail.com>
2023-09-21 13:19:12 +0300

    increase limit

--- backend/middlewares/rateLimit.js
+++ backend/middlewares/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
-	limit: 100,
+	limit: 10000,
 	standardHeaders: 'draft-7',
 	legacyHeaders: false,
 	message: 'Количество запросов на сервер превышено'
